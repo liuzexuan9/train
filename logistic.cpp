8f5909e070b7c11752bf6885bd2cdf9af5980346
liuzexuan9 <592011924@qq.com>
2020-04-09 23:41:05 +0800

    Update logistic.cpp
    编写logistic

--- logistic.cpp
+++ logistic.cpp
@@ -1,11 +1,70 @@
-double sigmoid(vector<double>&x,vector<double>&w)
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+class logistic
 {
-    double sum=0;
-    int n=x.size();
-    for(int i=0;i<n;i++)
+private:
+    int epochs=1000;
+    double learn_rate=0.01;
+double sigmoid(double z)
+{
+   
+    return 1.0/(1+exp(-z));
+}
+    //计算内积
+    double scaleProduct(vector<double>&x,vector<double>&y)
     {
-        sum+=(x[i]*w[i]);
+        double sum=0;
+        int n=x.size();
+        for(int i=0;i<n;i++)
+        {
+            sum+=(x[i]*y[i]);
+        }
+        return sum;
     }
-    
-    return 1.0/(1+exp(sum*-1));
+    double p(vector<double>&x,vector<double>&w)
+    {
+        return sigmoid(scaleProduct(x,w));
+    }
+    //梯度上升法求极大值
+public:
+    logistic(int epochs=1000,double learn_rate=0.01)
+    {
+        this->epochs=epochs;
+        this->learn_rate=learn_rate;
+    }
+    vector<double> grad_ascent(vector<vector<double>>&x,vector<int> &y)//二类分类，这里的y曲子{0,1}
+    {
+        int epoch=this->epochs;
+        int learn_rate=this->learn_rate;
+        int n=x.size();
+        int m=x[0].size();
+        vector<double> w(m,1);
+        vector<double>error(n);
+        
+        while(epochs--)
+        {
+            for(int i=0;i<n;i++)
+            {
+                error[i]=y[i]-p(x[i],w);
+                
+            }
+            for(int j=0;j<m;j++)
+            {
+                double sum=0;
+                for(int i=0;i<n;i++)
+                {
+                    sum+=x[i][j]*error[i];
+                }
+                w[j]+=learn_rate*sum;
+            }
+        }
+        return w;
+    }
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "这个程序是逻辑斯蒂的c++实现，先挖个坑，明天继续填坑\n";
+    return 0;
 }
